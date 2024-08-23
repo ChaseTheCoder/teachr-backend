4285baa3b12a824b8f356569674ffd8d1f2a0a17
Chase Sheaff <chasefsheaff@gmail.com>
2024-08-23 14:44:17 -0500

    authorization

--- plan/views.py
+++ plan/views.py
@@ -6,7 +6,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-# SUBJECT
 class SubjectList(APIView):
   def get(self, request, *args, **kwargs):
     queryset = Subject.objects.all()
