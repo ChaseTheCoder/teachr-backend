d800718e714776fd9801ff0c44e45bf6120c7f30
Chase Sheaff <chasefsheaff@gmail.com>
2024-09-11 21:22:25 -0500

    update settings page

--- teachr/settings.py
+++ teachr/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
