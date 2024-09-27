0f9cdd1acf6f8eb9bbe9afa05436a234255e31c5
Chase Sheaff <chasefsheaff@gmail.com>
2024-09-26 19:16:29 -0500

    remove plan app

--- plan/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Subject, Resource, UnitPlan, LessonPlan, Material
-
-admin.site.register(Subject)
-admin.site.register(UnitPlan)
-admin.site.register(Resource)
-admin.site.register(LessonPlan)
-admin.site.register(Material)
