c7801968ca8e1c58c90afc556279b48641e77d11
Chase Sheaff <chasefsheaff@gmail.com>
2024-10-14 14:11:22 -0500

    add SchoolClass and SchoolClassDay

--- schedules/serializer.py
+++ schedules/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from user_profile.models import UserProfile
-from .models import SchoolYear, SchoolDay
+from .models import SchoolClass, SchoolDayClass, SchoolYear, SchoolDay
 
 class SchoolYearSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,25 @@ class SchoolDaySerializer(serializers.ModelSerializer):
     class Meta:
         model = SchoolDay
         fields = '__all__'
+
+class SchoolDayWithClassesSerializer(serializers.ModelSerializer):
+    classes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SchoolDay
+        fields = '__all__'
+
+    def get_classes(self, obj):
+        classes = SchoolDayClass.objects.filter(school_day=obj)
+        return SchoolDayClassSerializer(classes, many=True).data
+
+class SchoolClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SchoolClass
+        fields = '__all__'
+
+class SchoolDayClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SchoolDayClass
+        fields = '__all__'
+
