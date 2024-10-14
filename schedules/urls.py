3b1e32c9755b7dc494b20d61d13fc8a2891b481a
Chase Sheaff <chasefsheaff@gmail.com>
2024-10-14 14:45:34 -0500

    add days_range_with_classes endpoint

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BulkCreateSchoolDayClassView, SchoolClassList, SchoolDayList, SchoolDayRange, SchoolYearDetail, SchoolYearList
+from .views import BulkCreateSchoolDayClassView, SchoolClassList, SchoolDayList, SchoolDayRange, SchoolDayRangeWithClasses, SchoolYearDetail, SchoolYearList
 
 urlpatterns = [
     path('schedules/<str:user_id>', SchoolYearList.as_view(), name='schedules'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('day/<str:school_day_id>', SchoolDayList.as_view(), name='school_day_delete'),
     path('days/<str:school_year_id>', SchoolDayList.as_view(), name='school_day'),
     path('days_range/<str:school_year_id>/', SchoolDayRange.as_view(), name='school_day_range'),
+    path('days_range_with_classes/<str:school_year_id>/', SchoolDayRangeWithClasses.as_view(), name='school_day_range'),
     path('school_classes/<str:school_year_id>', SchoolClassList.as_view(), name='school_classes'),
     path('bulk_create_school_day_classes/<str:school_year_id>', BulkCreateSchoolDayClassView.as_view(), name='bulk-create-school-day-classes'),
 ]
