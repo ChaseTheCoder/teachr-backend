d092d2e56bed43f9b8a29791066189f96cf3e37a
Chase Sheaff <chasefsheaff@gmail.com>
2024-11-21 19:31:34 -0600

    fix posts urls

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostByUser, PostFeed
 
 urlpatterns = [
-    path('posts/feed', PostFeed.as_view(), name='post-feed'),
-    path('posts/user/<str:user_id>', PostByUser.as_view(), name='posts-by-user'),
-    path('posts/<str:post_id>', PostByUser.as_view(), name='post-detail'),
+    path('posts/feed/', PostFeed.as_view(), name='post-feed'),
+    path('posts/user/<str:user_id>/', PostByUser.as_view(), name='posts-by-user'),
+    path('posts/<str:post_id>/', PostByUser.as_view(), name='post-detail'),
 ]
