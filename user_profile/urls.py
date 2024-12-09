4e56fbe31995bb13823fee4f280bcc8e35707fca
Chase Sheaff <chasefsheaff@gmail.com>
2024-12-09 15:17:28 -0600

    add profile batch

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from .views import UserProfileGetPost, UserProfilePicPatch, UserProfileUpdate
+from .views import UserProfileGetPost, UserProfilePicPatch, UserProfileUpdate, UserProfileBatchList
 
 
 urlpatterns = [
     path('profile_auth0/<str:auth0_id>', UserProfileGetPost.as_view(), name='user_profile_get_post'),
     path('profile/<str:id>', UserProfileUpdate.as_view(), name='user_profile_update'),
     path('profile_pic/<str:id>', UserProfilePicPatch.as_view(), name='user_profile_update'),
+    path('profile_batch/', UserProfileBatchList.as_view(), name='user_profile_update'),
 ]
 
 urlpatterns_str = str(urlpatterns)
