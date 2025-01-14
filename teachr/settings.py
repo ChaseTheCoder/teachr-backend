fff977ac6e116bddfb0c94cfb4aa672207b62fdb
Chase The Coder <65514507+ChaseTheCoder@users.noreply.github.com>
2025-01-14 10:28:24 -0600

    Update settings.py

--- teachr/settings.py
+++ teachr/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'debug_toolbar',
+    'corsheaders',
     'rest_framework',
     'plans',
     'user_profile',
