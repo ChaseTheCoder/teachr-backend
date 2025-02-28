7a900d4c1a6e59433bf950f9080313d6ebff48cb
Chase Sheaff <chasefsheaff@gmail.com>
2025-02-28 15:51:00 -0600

    add profile_pic_url with Render Disk attempt 1

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,5 +1,13 @@
 import uuid
+import os
 from django.db import models
+
+def user_profile_image_path(instance, filename):
+    # Get the file extension
+    ext = filename.split('.')[-1]
+    # Create filename as UUID
+    filename = f"{str(instance.id)}.{ext}"
+    return os.path.join('profile_pics', filename)
 
 class UserProfile(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
@@ -9,7 +17,11 @@ class UserProfile(models.Model):
   last_name = models.CharField(max_length=100, blank=True, default='')
   teacher_name = models.CharField(max_length=110, blank=True, default='')
   title = models.CharField(max_length=50, blank=True, default='')
-  profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+  profile_pic = models.ImageField(
+      upload_to=user_profile_image_path,
+      null=True,
+      blank=True
+  )
   verified = models.BooleanField(default=False)
   email_domain = models.CharField(max_length=254, blank=True, default='')
   verified_email = models.EmailField(max_length=254, blank=True, default='')
--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -2,11 +2,29 @@ from rest_framework import serializers
 from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    profile_pic_url = serializers.SerializerMethodField()
+
     class Meta:
         model = UserProfile
-        fields = ('id', 'created_at', 'auth0_id', 'first_name', 'last_name', 'teacher_name', 'title', 'verified', 'email_domain')
+        fields = ('id', 'created_at', 'auth0_id', 'first_name', 'last_name', 'teacher_name', 'title', 'verified', 'email_domain', 'profile_pic_url')
+
+    def get_profile_pic_url(self, obj):
+        if obj.profile_pic:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.profile_pic.url)
+        return None
 
 class BasicUserProfileSerializer(serializers.ModelSerializer):
+    profile_pic_url = serializers.SerializerMethodField()
+    
     class Meta:
         model = UserProfile
-        fields = ('id', 'teacher_name', 'title', 'verified')
+        fields = ('id', 'teacher_name', 'title', 'verified', 'profile_pic_url')
+
+    def get_profile_pic_url(self, obj):
+        if obj.profile_pic:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.profile_pic.url)
+        return None
