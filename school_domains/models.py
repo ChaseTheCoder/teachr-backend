617647021998e49c1391622d36760c6123c59432
Chase Sheaff <chasefsheaff@gmail.com>
2025-03-21 18:18:41 -0500

    add higher_ed to verified teachers

--- school_domains/models.py
+++ school_domains/models.py
@@ -2,6 +2,12 @@ from django.utils import timezone
 import uuid
 from django.db import models
 
+SCHOOL_TYPE_CHOICES = [
+    ('ECE', 'Early Childhood Center'),
+    ('K12', 'K-12'),
+    ('HIGHER_ED', 'Higher Education'),
+]
+
 class SchoolDomain(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     domain = models.CharField(max_length=255, unique=True)
@@ -9,7 +15,6 @@ class SchoolDomain(models.Model):
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     zip_code = models.CharField(max_length=255)
+    school_type = models.CharField(max_length=10, choices=SCHOOL_TYPE_CHOICES, default='K12')
+    staff_email_pattern = models.CharField(max_length=255, blank=True, null=True, help_text="Regex pattern for staff emails", default='.*@cps.edu')
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.domain
